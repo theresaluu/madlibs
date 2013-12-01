0d5a3e83996bab85cd7f3c5fa5f0631fc33383d8
Theresa Luu <theresa.luu@gmail.com>
2013-11-30 22:11:54 -0800

    modded config order to see if will fix friend issue on heroku

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,15 @@
 BDayBgosh::Application.routes.draw do
   root 'bgosh#index'
 
+  match 'auth/:provider/callback', to: 'sessions#create', via: [:get, :post]
+  match 'auth/failure', to: redirect('/'), via: [:get, :post]
+  match 'signout', to: 'sessions#destroy', as: 'signout', via: [:get, :post]
+
   namespace :api, defaults: {format: :json} do
     resources :wish_lists, only: [:index] do
       resources :wishes, only: [:index, :create, :update, :destroy]
     end
   end
-  match 'auth/:provider/callback', to: 'sessions#create', via: [:get, :post]
-  match 'auth/failure', to: redirect('/'), via: [:get, :post]
-  match 'signout', to: 'sessions#destroy', as: 'signout', via: [:get, :post]
+  
   
 end
