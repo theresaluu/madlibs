86f5087591bb5a1491f7dbbf92bb17810315eef4
Theresa Luu <theresa.luu@gmail.com>
2013-11-30 21:52:44 -0800

    modded friend/user name

--- app/models/friend.rb
+++ app/models/friend.rb
@@ -4,7 +4,7 @@ class Friend < ActiveRecord::Base
       friend.provider = auth.provider
       friend.uid = auth.uid
       friend.name = auth.info.name
-      friend.oath_token = auth.credentials.oath_token
+      friend.oath_token = auth.credentials.token
       friend.oath_expires_at = Time.at(auth.credentials.expires_at)
       friend.save!
     end
--- app/models/wish.rb
+++ app/models/wish.rb
@@ -1,5 +1,5 @@
 class Wish < ActiveRecord::Base
-	belongs_to :friend, class_name: 'Friend', foreign_key: 'friend_id'
+	belongs_to :user, class_name: 'User', foreign_key: 'user_id'
 
 end
     # t.string   "jname"
