46f7f7618aabaf622655810b58a6749e3383d8ba
Theresa Luu <theresa.luu@gmail.com>
2013-12-03 17:06:48 -0800

    sticky nav started

--- app/assets/javascripts/bgosh.js
+++ app/assets/javascripts/bgosh.js
@@ -1,16 +1,14 @@
 $(document).ready(function(){
- 
-    $('section[data-type="background"]').each(function(){
-      var $bgobj = $(this); // assigning the object
+  $('section[data-type="background"]').each(function(){
+    var $bgobj = $(this); // assigning the object
+    $(window).scroll(function() {
+    	var yPos = -($(window).scrollTop()/$bgobj.data('speed'));
 
-      $(window).scroll(function() {
-      	var yPos = -($(window).scrollTop()/$bgobj.data('speed'));
+    	// Put together our final background position
+    	var coords = '50% '+ yPos + 'px';
 
-      	// Put together our final background position
-      	var coords = '50% '+ yPos + 'px';
- 
-        // Move the background
-        $bgobj.css({ backgroundPosition: coords });
-      }); 
-    });
+      // Move the background
+      $bgobj.css({ backgroundPosition: coords });
+    }); 
+  });
 }); 
