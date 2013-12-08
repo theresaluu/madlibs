4c64fa70f39300150ea238d21c0b458920939d7c
Theresa Luu <theresa.luu@gmail.com>
2013-12-08 00:56:15 -0800

    a form shows- an ugly ass one

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 BDayBgosh::Application.routes.draw do
   root 'bgosh#index'
 
+  get "/friends/:friend_id/wishes/new" => 'wishes#new'
+
   match 'auth/:provider/callback', to: 'sessions#create', via: [:get, :post]
   match 'auth/failure', to: redirect('/'), via: [:get, :post]
   match 'signout', to: 'sessions#destroy', as: 'signout', via: [:get, :post]
