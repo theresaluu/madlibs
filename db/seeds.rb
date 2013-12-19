62f82e4ad0fed6ec49cb1fbbe9d1df76e2b10e09
Theresa Luu <theresa.luu@gmail.com>
2013-12-19 15:57:19 -0800

    name change from foundations/rails to something boov-related

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,4 +8,13 @@
 Friend.destroy_all
 Wish.destroy_all
 Theresa = Friend.create(name: 'Tbone')
-Theresa.wish = Wish.create(nickname: 'B\'Gosh', favmem: 'exchanging silly stories @Shangrila rooftop for Pizzle\'s bday', thought: 'cannot. stop. laughing... going to be friends with this guy for a long time.', first: 'more self-declared Josh Days', second: 'lots of completed goals', third: '#bombnoms', omen: 'heartburn', salutation: 'from the other end of the 405', relationship: 'BPF', wisher: 'tbone', ps: 'As one of your elders, my advice: keep doing what you are doing.  You are always motivating others to do better, super helpful, and a boatload of fun.  We all benefit from you being you.')
+Theresa.wish = Wish.create(nickname: 'B\'Gosh', 
+	favmem: 'exchanging silly stories @Shangrila rooftop for Pizzle\'s bday', 
+	thought: 'cannot. stop. laughing... going to be friends with this guy for a long time.', 
+	first: 'more self-declared Josh Days', 
+	second: 'lots of completed goals', 
+	third: '#bombnoms', omen: 'heartburn', 
+	salutation: 'from the other end of the 405', 
+	relationship: 'BPF', 
+	wisher: 'tbone', 
+	ps: 'As one of your elders, my advice: keep doing what you are doing.  You are always motivating others to do better, super helpful, and a boatload of fun.  We all benefit from you being you.')
